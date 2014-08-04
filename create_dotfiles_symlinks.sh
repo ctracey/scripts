5e4e9da4d3aa83162a9438736760e328ef403efb
Chris Tracey <tracey.chris@gmail.com>
2014-08-04 12:05:55 +1000

    create dotfiles symlinks

---
+++ create_dotfiles_symlinks.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ln -s .bash_profile /Users/christracey/dotfiles/.bash_profile
+ln -s .bashrc /Users/christracey/dotfiles/.bashrc
+ln -s .git-prompt.sh /Users/christracey/dotfiles/.git-prompt.sh
+ln -s .vim /Users/christracey/dotfiles/.vim/
+ln -s .vimrc /Users/christracey/dotfiles/.vimrc
