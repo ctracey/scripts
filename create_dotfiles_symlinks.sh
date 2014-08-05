037d25b34c0d3ac2abc1633c51dbfd11413428c0
Chris Tracey <tracey.chris@gmail.com>
2014-08-05 12:40:57 +1000

    updated symlinks

--- create_dotfiles_symlinks.sh
+++ create_dotfiles_symlinks.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-ln -s /Users/tracer/dotfiles/.bash_profile .bash_profile
-ln -s /Users/tracer/dotfiles/.bashrc .bashrc
-ln -s /Users/tracer/dotfiles/.git-prompt.sh .git-prompt.sh
-ln -s /Users/tracer/dotfiles/.vim/ .vim
-ln -s /Users/tracer/dotfiles/.vimrc .vimrc
-ln -s /Users/tracer/dotfiles/.ackrc .ackrc
-ln -s /Users/tracer/dotfiles/.gitconfig .gitconfig
-ln -s /Users/tracer/dotfiles/.gitignore .gitignore
+# ln -s /Users/tracer/dotfiles/.bash_profile .bash_profile
+# ln -s /Users/tracer/dotfiles/.bashrc .bashrc
+# ln -s /Users/tracer/dotfiles/.git-prompt.sh .git-prompt.sh
+# ln -s /Users/tracer/dotfiles/.vim/ .vim
+# ln -s /Users/tracer/dotfiles/.vimrc .vimrc
+# ln -s /Users/tracer/dotfiles/.ackrc .ackrc
+# ln -s /Users/tracer/dotfiles/.gitconfig .gitconfig
+# ln -s /Users/tracer/dotfiles/.gitignore .gitignore
+ln -s /Users/tracer/dotfiles/.ruby-version .ruby-version
