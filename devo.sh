0939fb511a8601c327273263435797b9c1cda915
Chris Tracey <tracey.chris@gmail.com>
2015-04-14 12:04:14 +1000

    devo default is git url

--- devo.sh
+++ devo.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+URL=`cat .git/config | grep url | awk {'a=sub(/git@github.com:/, "https://github.com/", $3); b=sub(/\.git/, "", $3); print $3'}`
+
 if [ $1 ]; then
   if [ $1 == "-g" ]; then
     URL=`cat .git/config | grep url | awk {'a=sub(/git@github.com:/, "https://github.com/", $3); b=sub(/\.git/, "", $3); print $3'}`
