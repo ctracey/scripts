0daeff02b6030d07e1d65f18d097dabf7149b843
Chris Tracey <tracey.chris@gmail.com>
2015-04-16 11:38:10 +1000

    devo commands

--- devo.sh
+++ devo.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+OPEN=true
 URL=`cat .git/config | grep url | awk {'a=sub(/git@github.com:/, "https://github.com/", $3); b=sub(/\.git/, "", $3); print $3'}`
 
 if [ $1 ]; then
@@ -13,7 +14,18 @@ if [ $1 ]; then
     URL=$URL/pulls
   elif [ $1 == "-b" ]; then
     URL=`pwd | awk {'a=sub(/^.*99designs\//, "https://buildkite.com/99designs/", $0); b=sub(/_/, "-", $0); print $0'}`
+  elif [ $1 == "-h" ]; then
+    OPEN=false
+    echo 'helpful commands for git repo'
+    echo "  default - open git repo in browser"
+    echo "  -g      - open git repo in browser"
+    echo "  -br     - open git branches in browser"
+    echo "  -pr     - open git pull requests in browser"
+    echo "  -b      - open buildkite build in browser"
+    echo "  -h      - help"
   fi
 fi
 
-open $URL
+if [ $OPEN == 'true' ]; then
+  open $URL
+fi
